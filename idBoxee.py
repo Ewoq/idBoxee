68cc07a9dabf9f77f50bb7e86d0e6a9e44f7698d
Ewoq <martinewon@gmail.com>
2013-06-20 17:28:44 +0200

    Added a GPL license to idBoxee.py

--- idBoxee.py
+++ idBoxee.py
@@ -1,3 +1,16 @@
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 def make_tvshow(title, id):
     file = open('tvshow.nfo', 'w')
     file.write('<tvshow>\n<title>' + title + '</title>\n<id>' + str(id) + '</id>\n</tvshow>')
@@ -31,9 +44,9 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-s','--search', help = 'List the name of the tv-show', type = str)
-    parser.add_argument('-t','--tvshow', help = 'List the name of the tv-show', type = str)
-    parser.add_argument('-m','--movie', help = 'List the name of the movie', type = str)
+    parser.add_argument('-s','--search', help = 'The title of the tv-show to search for', type = str)
+    parser.add_argument('-t','--tvshow', help = 'Title of the tv-show for the NFO file', type = str)
+    parser.add_argument('-m','--movie', help = 'Title of the movie for the NFO file', type = str)
     parser.add_argument('--id', help = 'The id of the tv-show or movie', type = int)
 
     args = parser.parse_args()
